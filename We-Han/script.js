7c1e7c3f55fbcb2b0599e4ab4d3f68774aac3bc8
Hanlord <chen007@live.at>
2022-06-01 14:39:01 +0200

    update

--- We-Han/script.js
+++ We-Han/script.js
@@ -1,17 +1,18 @@
-const btnAdd = document.querySelector("#result");
-const testBtn = document.querySelector(".test-btn");
-
-testBtn.onclick = printMessage.querySelector("#result");
-
-function validateInput (e){
-    console.log(e.target.value);
-    if (e.target.value.length < 5) {
-      e.target.style.color = "green";
-     
+function printFullName() {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let age = document.getElementById("age1").value;
+    let selector = document.getElementById("result");
+    if (fname.length > 5) {
+      selector.innerHTML = `<p id="text">${fname}</p><p id="text">${lname}</p><p id="number">${age}</p>`;
+      document.getElementById("text").style.color="green";
     } else {
-      e.target.style.color = "red";
-      
+      selector.innerHTML = `<p style="color:red">${fname}</p><p>${lname}</p>`;
+      document.getElementById("result").style.color = "red";
     }
-  };
-  validateInput(e);
+  }
+  document.getElementById("fname").addEventListener("input", printFullName);
+  document.getElementById("lname").addEventListener("input", printFullName);
+  document.getElementById("age1").addEventListener("input", printFullName);
 
+document.getElementById("btn").addEventListener("click", printFullName);
